7efba269f5fdead8c98f988a3e150c4edc38ee90
Sean Marinas <sean.marinas@s2t.ai>
2024-10-28 18:16:18 +0800

    added navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { NavigationMenuTrigger } from "@radix-ui/react-navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "./ui/navigation-menu";
+
+interface NavbarItem {
+  href: string;
+  label: string;
+}
+
+const navbarItems: NavbarItem[] = [
+  {
+    href: "#home",
+    label: "Home",
+  },
+  {
+    href: "#about",
+    label: "About",
+  },
+  {
+    href: "#works",
+    label: "Our Works",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <nav className="flex w-full p-[1rem]">
+      <div className="flex-grow">
+        <h1>Logo</h1>
+      </div>
+
+      <NavigationMenu>
+        <NavigationMenuList className="flex gap-[0.5rem]">
+          {navbarItems.map((item, index) => {
+            const { href, label } = item;
+
+            return (
+              <NavigationMenuItem key={index}>
+                <NavigationMenuTrigger
+                  onClick={() => {
+                    console.log(`Hello ${label}`);
+                  }}
+                >
+                  {label}
+                </NavigationMenuTrigger>
+              </NavigationMenuItem>
+            );
+          })}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </nav>
+  );
+}
