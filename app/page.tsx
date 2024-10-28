4412688500a21c814d02262c2469b23a12954cc3
Sean Marinas <sean.marinas@s2t.ai>
2024-10-28 18:57:43 +0800

    added about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutSection from "@/sections/AboutSection";
 import HomeSection from "../sections/HomeSection";
 
 export default function Home() {
   return (
-    <main className="flex flex-col p-[1rem] justify-center align-middle items-center w-full">
+    <main className="flex flex-col gap-[5rem] p-[1rem] justify-center align-middle items-center w-full">
       <HomeSection />
+      <AboutSection />
     </main>
   );
 }
---
+++ sections/AboutSection.tsx
@@ -0,0 +1,31 @@
+export default function AboutSection() {
+  return (
+    <section className="text-5xl text-left w-full flex flex-col gap-[3rem] font-bold">
+      <div className="flex flex-col gap-[0.75rem]">
+        <p>At Ui-Li Print & Services we</p>
+        <p>
+          <span className="text-primary">elevate brands</span> by delivering
+        </p>
+        <p className="text-primary">exceptional printing solutions.</p>
+      </div>
+
+      <div className="flex flex-col gap-[0.75rem]">
+        <p>Our mission is simple</p>
+        <p>to add value through</p>
+        <p>
+          <span className="text-primary">personlaised service </span>
+          and
+        </p>
+        <p className="text-primary">quality craftsmanship.</p>
+      </div>
+
+      <div className="flex flex-col gap-[0.75rem]">
+        <p>From design to delivery</p>
+        <p>our fully equipped facility</p>
+        <p>ensures your projects</p>
+        <p>stand out and reach you</p>
+        <p>seamlessly.</p>
+      </div>
+    </section>
+  );
+}
