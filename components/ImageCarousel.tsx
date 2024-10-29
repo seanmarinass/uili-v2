2e501d3fdd3f3e4119da1a051374acc17b08c868
Sean Marinas <sean.marinas@s2t.ai>
2024-10-29 18:47:06 +0800

    added moe logo to clients

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -60,6 +60,17 @@ export default function ImageCarousel() {
             />
           </Card>
         </div>
+
+        <div className="embla__slide">
+          <Card className="relative w-[500px] h-[500px] aspect-square bg-white text-black flex justify-center align-middle items-center">
+            <Image
+              src="/moeLogo.jpg"
+              alt="Ministry of Edication logo"
+              fill={true}
+              style={{ objectFit: "contain" }}
+            />
+          </Card>
+        </div>
       </div>
     </div>
   );
--- sections/ClientSection.tsx
+++ sections/ClientSection.tsx
@@ -3,11 +3,10 @@ import ImageCarousel from "@/components/ImageCarousel";
 export default function ClientSection() {
   return (
     <section className="flex w-full text-7xl font-bold justify-center align-middle items-center">
-      <div className="text-left w-[50%]">
+      <ImageCarousel />
+      <div className="text-center w-[50%]">
         <p>Our Clients</p>
       </div>
-
-      <ImageCarousel />
     </section>
   );
 }
